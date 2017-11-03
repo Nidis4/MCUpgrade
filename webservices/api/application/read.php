a105cf0c560773890aacefec049f8cff3f1d2aa1
Nidis4 <nidis4@gmail.com>
2017-11-03 17:00:27 +0200

    Applications Upload

---
+++ webservices/api/application/read.php
@@ -0,0 +1,63 @@
+<?php
+// required header
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ 
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/application.php';
+ 
+// instantiate database and category object
+$database = new Database();
+$db = $database->getConnection();
+ 
+// initialize object
+$application = new Appication($db);
+ 
+// query categorys
+$stmt = $application->read();
+$num = $stmt->rowCount();
+ 
+// check if more than 0 record found
+if($num>0){
+ 
+    // products array
+    $applications_arr=array();
+    $applications_arr["records"]=array();
+ 
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+ 
+        $application_item=array(
+            "id" => $id,
+            "category_id" => $category_id,
+            "title" => $title,
+            "title_greek" => $title_greek,
+            "short_description" => html_entity_decode($short_description),
+            "short_description_gr" => html_entity_decode($short_description_gr),
+            "detail_description" => html_entity_decode($detail_description),
+            "detail_description_gr" => html_entity_decode($detail_description_gr),
+            "unit" => $unit,
+            "min_price" => $min_price,
+            "sequence" => $sequence,
+            "modified" => $modified
+        );
+ 
+        array_push($applications_arr["records"], $application_item);
+    }
+ 
+    echo json_encode($applications_arr);
+}
+ 
+else{
+    echo json_encode(
+        array("message" => "No products found.")
+    );
+}
+?>
---
+++ webservices/api/objects/application.php
@@ -0,0 +1,43 @@
+<?php
+class Application{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "applications";
+ 
+    // object properties
+    public $id;
+    public $category_id
+    public $title;
+    public $title_greek;
+    public $short_description;
+    public $short_description_gr;
+    public $detail_description;
+    public $detail_description_gr;
+    public $unit;
+    public $min_price;
+    public $sequence;
+    public $modified;
+
+ 
+    public function __construct($db){
+        $this->conn = $db;
+    }
+ 
+    // used by select drop-down list
+    public function read(){
+        //select all data
+        $query = "SELECT
+                    id, category_id, title, title_greek, short_description, short_description_gr, detail_description, detail_description_gr, unit, min_price, sequence, modified
+                FROM
+                    " . $this->table_name . "
+                ORDER BY
+                    id ASC";
+ 
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+ 
+        return $stmt;
+    }
+}
+?>
