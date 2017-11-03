41f412b5b445a3c6fdcd9f5aff797bc48fd56885
Nidis4 <nidis4@gmail.com>
2017-11-03 14:49:53 +0200

    Admin Read

---
+++ webservices/api/admin/read.php
@@ -0,0 +1,64 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ 
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/admins.php';
+ 
+// instantiate database and admin object
+$database = new Database();
+$db = $database->getConnection();
+ 
+// initialize object
+$admin = new Admin($db);
+ 
+// query Admins
+$stmt = $admin->read();
+$num = $stmt->rowCount();
+ 
+// check if more than 0 record found
+if($num>0){
+ 
+    // admins array
+    $admins_arr=array();
+    $admins_arr["records"]=array();
+ 
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+ 
+        $admin_item=array(
+            "id" => $id,
+            "username" => $username,
+            "password" => $password,
+            "first_name" => $first_name,
+            "last_name" => $last_name,
+            "email" => $email,
+            //"description" => html_entity_decode($description),
+            "mobile_nr" => $mobile_nr,
+            "password_changed" => $password_changed,
+            "sms_code" => $sms_code,
+            "type" => $type,
+            "last_login" => $last_login,
+            "active" => $active,
+        );
+ 
+        array_push($admin_arr["records"], $admin_item);
+    }
+ 
+    echo json_encode($admin_arr);
+}
+ 
+else{
+    echo json_encode(
+        array("message" => "No Admins found.")
+    );
+}
+?>
---
+++ webservices/api/objects/admin.php
@@ -0,0 +1,45 @@
+<?php
+class Admin{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "admin";
+ 
+    // object properties
+    public $id;
+    public $username;
+    public $password;
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $mobile_nr;
+    public $password_changed;
+    public $sms_code;
+    public $type;
+    public $last_login;
+    public $active;
+    public $created;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    // read products
+    function read(){
+     
+        // select all query
+        $query = "SELECT  a.id, a.username, a.password, a.first_name, a.last_name, a.email, a.mobile_nr, a.password_changed, a.sms_code, a.type, a.last_login, a.active, a.created
+                FROM " . $this->table_name . " p
+                ORDER BY
+                    p.created DESC";
+     
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+     
+        // execute query
+        $stmt->execute();
+     
+        return $stmt;
+    }
+}
