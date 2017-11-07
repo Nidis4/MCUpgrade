c11b158d8ecd0d64732ea1171602db77a3ed14d5
Nidis4 <nidis4@gmail.com>
2017-11-07 13:36:27 +0200

    Change to members

--- webservices/api/member/contactDetails.php
+++ webservices/api/member/contactDetails.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json; charset=UTF-8");
 // include database and object files
 include_once '../config/database.php';
 include_once '../config/core.php';
-include_once '../objects/application.php';
+include_once '../objects/members.php';
  
 // instantiate database and category object
 $database = new Database();
