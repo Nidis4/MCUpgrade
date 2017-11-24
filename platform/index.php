6cf9762e7d632b60dbcc723835b351546deaa51f
Nidis4 <nidis4@gmail.com>
2017-11-24 17:42:52 +0200

    Logout
    Unset Session

--- platform/index.php
+++ platform/index.php
@@ -1,6 +1,5 @@
 <?php
 include('session.php');
-echo "Session: ".$_SESSION['login_user'];
 ?>
 <!doctype html>
 <html class="fixed sidebar-left-collapsed">
@@ -17,29 +16,9 @@ echo "Session: ".$_SESSION['login_user'];
 		<!-- Mobile Metas -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-		<!-- Web Fonts  -->
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
-
-		<!-- Vendor CSS -->
-		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css" />
-		<link rel="stylesheet" href="vendor/animate/animate.css">
-
-		<link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.css" />
-		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.css" />
-		<link rel="stylesheet" href="vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css" />
-
-		<!-- Specific Page Vendor CSS -->
-		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.css" />
-		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.css" />
-
-		<!-- Theme CSS -->
-		<link rel="stylesheet" href="css/theme.css" />
-
-		<!-- Skin CSS -->
-		<link rel="stylesheet" href="css/skins/default.css" />
-
-		<!-- Theme Custom CSS -->
-		<link rel="stylesheet" href="css/custom.css">
+		<?php
+		include('includes/css.php');
+		?>
 
 		<!-- Head Libs -->
 		<script src="vendor/modernizr/modernizr.js"></script>
@@ -52,7 +31,7 @@ echo "Session: ".$_SESSION['login_user'];
 			<header class="header">
 				<div class="logo-container">
 					<a href="../2.0.0" class="logo">
-						<img src="img/logo.png" width="75" height="35" alt="Porto Admin" />
+						<img src="img/logo.png" width="162" height="47" alt="Porto Admin" />
 					</a>
 					<div class="d-md-none toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
 						<i class="fa fa-bars" aria-label="Toggle sidebar"></i>
@@ -259,7 +238,7 @@ echo "Session: ".$_SESSION['login_user'];
 									<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="fa fa-lock"></i> Lock Screen</a>
 								</li>
 								<li>
-									<a role="menuitem" tabindex="-1" href="pages-signin.html"><i class="fa fa-power-off"></i> Logout</a>
+									<a role="menuitem" tabindex="-1" href="logout.php"><i class="fa fa-power-off"></i> Logout</a>
 								</li>
 							</ul>
 						</div>
