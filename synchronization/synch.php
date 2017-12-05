921748932055de185c79e5fc386a38bee253e0f3
Nidis4 <nidis4@gmail.com>
2017-12-05 18:10:31 +0200

    sync Applications

--- synchronization/synch.php
+++ synchronization/synch.php
@@ -12,6 +12,8 @@ $live_db_pass = 'u~,oEFS]5b}I';
 
 echo "Synchronization Starts<br>";
 syncCategories();
+syncApplications();
+
 
 function syncCategories(){
 	echo "In Sync Categories<br>";
@@ -22,7 +24,6 @@ function syncCategories(){
 
 	    /* fetch associative array */
 	    while ($row = $result->fetch_assoc()) {
-	        //printf ("%s (%s)\n", $row["name"], $row["name_greek"]);
 	        insertCategories($row['id'], $row['name'], $row['name_greek'], $row['title'], $row['title_greek'], $row['description'], $row['description_greek'], $row['sequence'], $row['modified'], $row['commissionRate']);
 	    }
 
@@ -35,7 +36,34 @@ function insertCategories($id, $name, $name_greek, $title, $title_greek, $descri
 	echo "Inserting Category ".$id."<br>";
 
 	$query = "INSERT INTO `categories` ( `id`, `name`, `name_greek`, `title`, `title_greek`, `description`, `description_greek`, `sequence`, `modified`, `commissionRate`) VALUES (".$id.",'".$name."','".$name_greek."','".$title."','".$title_greek."','".$description."','".$description_greek."','".$sequence."','".$modified."','".$commissionRate."') ON DUPLICATE KEY UPDATE `name`='".$name."', `name_greek`='".$name_greek."', `title`='".$title."', `title_greek`='".$title_greek."', `description`='".$description."', `description_greek`='".$description_greek."', `sequence`='".$sequence."', `modified`='".$modified."', `commissionRate`='".$commissionRate."' ";
-	echo $query;
+	//echo $query;
+
+	$upgrade = UpgradeDB();
+	$result = $upgrade->query($query);
+}
+
+function syncApplications(){
+	echo "In Sync Applications<br>";
+	$query = "SELECT `id`, `category_id`, `title`, `title_greek`, `short_description`, `short_description_gr`, `detail_description`, `detail_description_gr`, `unit`, `min_price`, `sequence`, `modified` FROM `applications`";
+
+	$live = LiveDB();
+	if ($result = $live->query($query)) {
+
+	    /* fetch associative array */
+	    while ($row = $result->fetch_assoc()) {
+	        insertApplications($row['id'], $row['category_id'], $row['title'], $row['title_greek'], $row['short_description'], $row['short_description_gr'], $row['detail_description'], $row['detail_description_gr'], $row['unit'], $row['min_price'], $row['sequence'], $row['modified']);
+	    }
+
+	    /* free result set */
+	    $result->free();
+	}
+}
+
+function insertApplications($id, $category_id, $title, $title_greek, $short_description, $short_description_gr, $detail_description, $detail_description_gr, $unit, $min_price, $sequence, $modified){
+	echo "Inserting Applications ".$id."<br>";
+
+	$query = "INSERT INTO `applications` ( `id`, `category_id`, `title`, `title_greek`, `short_description`, `short_description_gr`, `detail_description`, `detail_description_gr`, `unit`, `min_price`, `sequence`, `modified`) VALUES (".$id.",'".$category_id."','".$title."','".$title_greek."','".$short_description."','".$short_description_gr."','".$detail_description."','".$detail_description_gr."','".$unit."','".$min_price."', '".$sequence."', '".$modified."') ON DUPLICATE KEY UPDATE `category_id`='".$category_id."', `title`='".$title."', `title_greek`='".$title_greek."', `short_description`='".$short_description."', `short_description_gr`='".$short_description_gr."', `detail_description`='".$detail_description."', `detail_description_gr`='".$detail_description_gr."', `unit`='".$unit."', `min_price`='".$min_price."', `sequence`='".$sequence."', `modified`='".$modified."' ";
+	//echo $query;
 
 	$upgrade = UpgradeDB();
 	$result = $upgrade->query($query);
