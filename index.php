273c5f97a443f90c627446e93ea2be1486d20072
giannis pragias <giannis.pragias@gmail.com>
2018-02-01 10:57:57 +0200

    home page
    home page

---
+++ index.php
@@ -0,0 +1,537 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../../favicon.ico">
+	<title>ΜyConstructor</title>
+
+	<link href="assets/bootstrap337/css/bootstrap.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!-- <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <script src="assets/bootstrap337/js/bootstrap.min.js"></script> 
+
+       <!-- Custom styles -->
+    <!--<link rel="stylesheet" href="social-likes_flat.css">-->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<link rel="stylesheet" type="text/css" href="css/style.css?version=24">
+  	<link rel="stylesheet" type="text/css" href="css/sidenav.css">
+
+
+  	<script src="js/home.js"></script>
+</head>
+<body>
+
+	<div class="container container-home-slider">
+				<nav id="myTopnav" class="navbar navbar-default">
+					
+						  <div class="container-fluid">
+						    <!-- Brand and toggle get grouped for better mobile display -->
+						    <div class="navbar-header">
+						      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						        <span class="sr-only">Toggle navigation</span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						      </button>
+						     <a class="navbar-brand" href="https://myconstructor.gr"><img src="img/home-page/logo-mcr-310.png" /></a>
+						    </div>
+						    <!-- Collect the nav links, forms, and other content for toggling -->
+						    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						      <ul class="nav navbar-nav">
+						        <li><a href="https://myconstructor.gr/">ΑΡΧΙΚΗ</a></li>
+						        <li><a href="https://myconstructor.gr/homes/search_constructors/all/all/all/126/177">ΚΑΤΑΛΟΓΟΣ ΕΠΑΓΓΕΛΜΑΤΙΩΝ</a></li>
+						        <!--<li class="dropdown">
+						        	<a class="dropdown-toggle" data-toggle="dropdown" href="#">Υπηρεσίες Ηλεκτρολόγου
+						        	<span class="caret"></span></a>
+							        <ul class="dropdown-menu">
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/ilektrologikos-pinakas.php">Ηλεκτρολογικός Πίνακας</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/ilektrologikes-ergasies.php">Αλλαγή Πρίζας, Διακόπτη</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/ilektrologikes-ergasies.php">Θερμοσίφωνας</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/fwtismos.php">Φωτισμός</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/diktya.php">Εγκατάσταση Δικτύων</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/thirotilefona.php">Θυροτηλέφωνο</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/leukes-siskeues.php">Επισκευή Λευκών Συσκευών</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/sunagermos.php">Συστήματα Ασφαλείας</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/energeiako-pistopoihtiko.php">Ενεργειακό Πιστοποιητικό ΠΕΑ</a></li>
+							          <li><a href="https://development.myconstructor.gr/services/ilektrologos/pistopoihtiko-deh.php">Πιστοποιητικό ΔΕΗ</a></li>
+							        </ul>
+						      	</li>-->
+						        <li><a href="https://myconstructor.gr/blog/">BLOG</a></li>
+						        <li class="active litel"><a href="tel:2103009323">210 300 9323</a></li>
+						        <li><a href="https://myconstructor.gr/blog/">ΕΝ</a></li>
+						        <li><a href="https://myconstructor.gr/blog/">ΕL</a></li>
+						      </ul>
+						      <ul class="nav navbar-nav navbar-right">
+						        <li><a href="https://myconstructor.gr/members/chooseUserType"><span class="glyphicon glyphicon-user"></span> ΕΓΓΡΑΦΗ</a></li>
+						        <li><a href="https://myconstructor.gr/members/login"><span class="glyphicon glyphicon-log-in"></span> ΣΥΝΔΕΣΗ</a></li>
+						      </ul>
+						    </div><!-- /.navbar-collapse -->
+						  </div><!-- /.container-fluid -->
+						
+				</nav>
+
+			
+				<div class="row">
+					<div class="col-md-12">
+						<h1>Όλες οι τεχνικές εργασίες</h1>
+						<p>Βρες Αξιολογημένους & Οικονομικούς επαγγελματίες για όλες τις εργασίες που αφορούν το σπίτι και την οικοδομή!</p>
+
+						<div class="searcharea">
+							
+							<input id="inputsearch" type="text" name="" placeholder="Γράψε την εργασία που χρειάζεσαι.." onclick="search()">
+						</div>
+
+					</div>
+					
+				</div>
+	</div>
+	<div class="container cats-container">
+		<div class="row display-table">
+			<div class="col-md-6">
+						<div class="first-half">
+							<h1>Τί είναι το Lorem!</h1>
+							<div class="div-wave"><img src="img/home-page/wave-line.png"></div>
+							<p>Το Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας</p>
+
+							<div class="btn-cat">ΚΑΤΑΛΟΓΟΣ ΕΠΑΓΓΕΛΜΑΤΙΩΝ</div>
+						</div>
+			</div>
+			<div class="col-md-6">
+				<div class="cleaning-outer">
+					<div class="cleaning-first-half">
+						
+					</div>
+					<div class="cleaning-second-half">
+						<div class="box-img">
+							<img src="img/home-page/cleaning-icon.png">
+						</div>
+						<h2>Συνεργεία καθαρισμού</h2>
+						<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+
+		<div class="row display-table">
+			<div class="col-md-6">
+				<div class="mover-outer">
+					<div class="mover-first-half">
+						
+					</div>
+					<div class="mover-second-half">
+						<div class="box-img">
+							<img src="img/home-page/mover-box-icon.png">
+						</div>
+						<h2>Μεταφορές & Μετακομίσεις</h2>
+						<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+						<div class="btn-cat btn-cat-center">ΜΕΤΑΦΟΡΙΚΗ ΕΤΑΙΡΕΙΑ</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="outer-testimon">
+					<div class="testimon1-first-half">
+							<div class="box-img">
+								<img src="img/home-page/quote.png">
+							</div>
+							<h3>I would recommend this company to anyone needing great service</h3>
+							<p>– George Ianapoulous</p>
+					</div>
+					
+					<div class="testimon1-second-half">
+							
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row display-table">
+			<div class="col-md-6">
+				<div class="mover-outer">
+					<div class="bars">
+						<div class="scrillbar-content">
+							<div class="skillbar-title"><span>Ποιότητα Εργασιών</span></div>
+							<div class="skill-bar-percent">90%</div>
+						</div>
+						
+						<div class="skillbar  clearfix" data-percent="90%">
+							<div class="skillbar-bar" style="background-color: rgb(255, 227, 97);"></div>
+						</div> <!-- End Skill Bar -->
+						<div class="scrillbar-content">
+							<div class="skillbar-title"><span>Σχέση Ποιότητας Τιμής Εργασιών</span></div>
+							<div class="skill-bar-percent">95%</div>
+						</div>
+						<div class="skillbar clearfix " data-percent="95%">
+							<div class="skillbar-bar" style="background-color: rgb(255, 227, 97);"></div>
+						</div> <!-- End Skill Bar -->
+
+						<div class="scrillbar-content">
+							<div class="skillbar-title"><span>Συνέπεια</span></div>
+							<div class="skill-bar-percent">92%</div>
+						</div>
+						<div class="skillbar clearfix " data-percent="92%">
+							<div class="skillbar-bar" style="background-color: rgb(255, 227, 97);"></div>
+						</div> <!-- End Skill Bar -->
+
+						<div class="scrillbar-content">
+							<div class="skillbar-title"><span>Συμπεριφορά</span></div>
+							<div class="skill-bar-percent">93%</div>
+						</div>
+						<div class="skillbar clearfix " data-percent="93%">
+							<div class="skillbar-bar" style="background-color: rgb(255, 227, 97);"></div>
+						</div> <!-- End Skill Bar -->
+
+						<div class="scrillbar-content">
+							<div class="skillbar-title"><span>Καθαριότητα</span></div>
+							<div class="skill-bar-percent">90%</div>
+						</div>	
+						<div class="skillbar clearfix " data-percent="90%">
+							<div class="skillbar-bar" style="background-color: rgb(255, 227, 97);"></div>
+						</div> 
+
+						<!--<div class="scrillbar-content">
+							<div class="skillbar-title"><span>SEO</span></div>
+							<div class="skill-bar-percent">100%</div>
+						</div>
+						<div class="skillbar clearfix " data-percent="100%">
+							<div class="skillbar-bar" style="background-color: rgb(255, 227, 97);"></div>
+						</div> 
+
+						<div class="scrillbar-content">
+							<div class="skillbar-title"><span>Photoshop</span></div>
+							<div class="skill-bar-percent">70%</div>
+						</div>
+						<div class="skillbar clearfix " data-percent="70%">	
+							<div class="skillbar-bar" style="background-color: rgb(255, 227, 97);"></div>
+						</div> -->
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="outer-testimon">
+
+					<div class="workers-first-half">
+							
+					</div>
+					<div class="workers-second-half">
+							<p class="over-title">ΚΕΙΜΕΝΟ ΧΩΡΙΣ ΝΟΗΜΑ</p>
+							<h2>Ανακαίνιση Σπιτιού</h2>
+							<div class="separator"></div>
+							<p>Είναι πλέον κοινά παραδεκτό ότι ένας αναγνώστης αποσπάται από το περιεχόμενο που διαβάζει</p>
+							<div class="btn-cat btn-cat-center">ΠΑΚΕΤΑ ΑΝΑΚΑΙΝΙΣΗΣ</div>
+					</div>
+					
+				
+				</div>
+			</div>
+		</div>
+
+
+		<div class="row display-table">
+			<div class="col-md-6">
+				<div class="mover-outer">
+					<div class="painter-first-half">
+						
+					</div>
+					<div class="mover-second-half">
+						<div class="box-img">
+							<img src="img/home-page/brush-roller.png">
+						</div>
+						<h2>Ελαιοχρωματισμοί</h2>
+						<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="outer-testimon">
+					
+					<div class="tiles-first-half">
+							<div class="box-img">
+								<img src="img/home-page/trowel-1.png" />
+							</div>
+							<h2>Τοποθέτηση Δαπέδων</h2>
+							<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+							<div class="btn-cat btn-cat-center">ΤΟΠΟΘΕΤΗΣΗ ΔΑΠΕΔΩΝ</div>
+					</div>
+					<div class="tiles-second-half">
+							
+					</div>
+					
+					
+				</div>
+			</div>
+		</div>
+
+
+		<div class="row display-table">
+			<div class="col-md-6">
+				<div class="mover-outer">
+					
+					<div class="electr-second-half">
+					
+							<div class="box-img">
+								<img src="img/home-page/el-icon.png">
+							</div>
+							<h2>Ηλεκτρολογικά</h2>
+							<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+						
+					</div>
+
+					<div class="electr-first-half">
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="outer-testimon">
+					<div class="idraulika-second-half">
+							
+					</div>
+					
+					<div class="idraulika-first-half">
+							<div class="box-img">
+								<img src="img/home-page/plumber-icon.png" />
+							</div>
+							<h2>Υδραυλικά</h2>
+							<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+							
+					</div>
+					
+					
+					
+				</div>
+			</div>
+		</div>
+
+
+		<div class="row display-table">
+			
+			<div class="col-md-6">
+				<div class="outer-testimon">
+					<div class="pistopoihtika-second-half">
+						<div class="box-img">
+								<img src="img/home-page/deh-icon.png" />
+							</div>
+							<h3>Πιστοποιητικό Δ.Ε.Η.</h3>
+							<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+						
+					</div>
+					
+					<div class="pistopoihtika-first-half">
+							<div class="box-img">
+								<img src="img/home-page/energy-cert.png" />
+							</div>
+							<h3>Ενεγργειακό Πιστοποιητικό</h3>
+							<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+
+							
+					</div>
+
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="pistopoihtika-outer">
+					
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="row row-texnikos display-table" >
+
+			<div class="col-md-6">
+				<div class="texnikos-outer">
+					
+					
+				</div>
+			</div>
+
+
+			
+			<div class="col-md-6">
+				<div class="outer-testimon">
+					<div class="texnikos-second-half">
+						<div class="box-img">
+								<img src="img/home-page/texnikos-asfaleias-1.png" />
+							</div>
+							<h3>Τεχνικός Ασφαλείας</h3>
+							<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+						
+					</div>
+					
+					<div class="texnikos-first-half">
+							<div class="box-img">
+								<img src="img/home-page/quote.png" />
+							</div>
+							<h3>I would recommend this company to anyone needing great service</h3>
+							<p>– George Ianapoulous</p>
+
+							
+					</div>
+
+				</div>
+			</div>
+
+			
+
+		</div>
+
+
+		<div class="row display-table">
+			<div class="col-md-6">
+				<div class="mover-outer">
+					<div class="aircon-first-half">
+						
+					</div>
+					<div class="aircon-second-half">
+						<div class="box-img">
+							<img src="img/home-page/aircondition-icon-1.png">
+						</div>
+						<h3>Εγκατάσταση & Συντήρηση Κλιματιστικού</h3>
+						<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="outer-testimon">
+					
+					<div class="kau-first-half">
+							<div class="box-img">
+								<img src="img/home-page/tools.png" />
+							</div>
+							<h3>Επισκευή Λευκών Συσκευών</h3>
+							<p>ο Lorem Ipsum είναι απλά ένα κείμενο χωρίς νόημα για τους επαγγελματίες της τυπογραφίας και στοιχειοθεσίας.</p>
+							<div class="btn-cat btn-cat-center">ΕΠΙΣΚΕΥΗ ΛΕΥΚΩΝ ΣΥΣΚΕΥΩΝ</div>
+					</div>
+					<div class="kau-second-half">
+							
+					</div>
+					
+					
+				</div>
+			</div>
+		</div>
+		<i class="mkd-icon-font-awesome fa fa-twitter mkd-icon-element" style=""></i>
+
+
+
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+<!--<div class="skillbar clearfix " data-percent="20%">
+	<div class="skillbar-title" style="background: #d35400;"><span>HTML5</span></div>
+	<div class="skillbar-bar" style="background: #e67e22;"></div>
+	<div class="skill-bar-percent">20%</div>
+</div> 
+
+<div class="skillbar clearfix " data-percent="25%">
+	<div class="skillbar-title" style="background: #2980b9;"><span>CSS3</span></div>
+	<div class="skillbar-bar" style="background: #3498db;"></div>
+	<div class="skill-bar-percent">25%</div>
+</div> 
+
+<div class="skillbar clearfix " data-percent="50%">
+	<div class="skillbar-title" style="background: #2c3e50;"><span>jQuery</span></div>
+	<div class="skillbar-bar" style="background: #2c3e50;"></div>
+	<div class="skill-bar-percent">50%</div>
+</div> 
+
+<div class="skillbar clearfix " data-percent="40%">
+	<div class="skillbar-title" style="background: #46465e;"><span>PHP</span></div>
+	<div class="skillbar-bar" style="background: #5a68a5;"></div>
+	<div class="skill-bar-percent">40%</div>
+</div> 
+
+<div class="skillbar clearfix " data-percent="75%">
+	<div class="skillbar-title" style="background: #333333;"><span>Wordpress</span></div>
+	<div class="skillbar-bar" style="background: #525252;"></div>
+	<div class="skill-bar-percent">75%</div>
+</div> 
+
+<div class="skillbar clearfix " data-percent="100%">
+	<div class="skillbar-title" style="background: #27ae60;"><span>SEO</span></div>
+	<div class="skillbar-bar" style="background: #2ecc71;"></div>
+	<div class="skill-bar-percent">100%</div>
+</div>
+
+<div class="skillbar clearfix " data-percent="70%">
+	<div class="skillbar-title" style="background: #124e8c;"><span>Photoshop</span></div>
+	<div class="skillbar-bar" style="background: #4288d0;"></div>
+	<div class="skill-bar-percent">70%</div>
+</div> End Skill Bar -->
+
+	<!--<div class="container">
+		    <div class="row display-table">
+		        <div class="col-lg-4">
+		            <div class="box">
+		                <h3>Some text</h3>
+		                <img src="//placehold.it/150x180" class="center-block">
+		            </div>
+		        </div>
+		        <div class="col-lg-4">
+		            <div class="box">
+		                <h3>Some text</h3>
+		                <img src="//placehold.it/150x150" class="center-block">
+		            </div>
+		        </div>
+		        <div class="col-lg-4">
+		            <div class="box">
+		                <h3>Some text more text that wraps to new link</h3>
+		                <img src="//placehold.it/150x150" class="center-block">
+		            </div>
+		        </div>
+		        <div class="col-lg-4">
+		            <div class="box">
+		                <h3>Some text</h3>
+		                <img src="//placehold.it/150x150" class="center-block">
+		            </div>
+		        </div>
+		        <div class="col-lg-4">
+		            <div class="box">
+		                <h3>Some text</h3>
+		                <img src="//placehold.it/150x150" class="center-block">
+		            </div>
+		        </div>
+		        <div class="col-lg-4">
+		            <div class="box">
+		                <h3>Some text</h3>
+		                <img src="//placehold.it/150x150" class="center-block">
+		            </div>
+		        </div>
+		    </div>
+	</div>-->
+		
+
+		
+
+
+
+</body>
+
+
+
+</html>
