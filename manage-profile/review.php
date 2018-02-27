d0bd94c3820f1bec275c51876228cb2ac3e404f8
giannis pragias <giannis.pragias@gmail.com>
2018-02-27 12:27:47 +0200

    proffessioanl prifile timeline

---
+++ manage-profile/review.php
@@ -0,0 +1,54 @@
+<?php 
+    include('header.php');
+    $currentPage = 'review';
+    include('menu.php');
+        ?>
+                     <div class="row">
+                        <div class="col-md-12 page-title-review"><h2>Customer Reviews</h2></div>
+                            <div class="col-md-6">
+                                <div class="reviewsTitle">
+                                    <div class="TotalRatingOuter"><span class="totalRatingReviews">5</span>/5 </div><div class="total-score">Total score <br>
+                                            Βαθμός από <span class="total-jobs">190</span> Αξιολογήσεις </div>
+                                </div>
+                                <div class="totalScore">
+                                    <div class="totalStars">
+                                        <div style="width: 100%;"></div>
+                                    </div>
+                                </div>
+                            </div>
+                                                
+                                            
+                            <div class="col-md-6">
+                                                            
+                                <p class="emplexp">Ποιότητα</p>
+                                   <div class="quality-empty">
+                                        <div style="width:98%;"></div>
+                                    </div>
+                                 <p class="emplexp">Αξιοπιστία</p>
+                                    <div class="reliability-empty">
+                                        <div style="width:98%;"></div>
+                                    </div>
+                                 <p class="emplexp">Κόστος</p>
+                                    <div class="cost-empty">
+                                        <div style="width:98%;"></div>
+                                    </div>
+                                 <p class="emplexp">Συνέπεια</p>
+                                    <div class="schedule-empty">
+                                        <div style="width:98%;"></div>
+                                    </div>
+                                 <p class="emplexp">Συμπεριφορά</p>
+                                    <div class="behaviour-empty">
+                                        <div style="width:98%;"></div>
+                                    </div>
+                                 <p class="emplexp">Καθαριότητα</p>
+                                    <div class="cleanliness-empty">
+                                        <div style="width:98%;"></div>
+                                    </div>
+                             </div>
+                        </div>  
+           
+           
+        </div>
+
+    </body>
+</html>
