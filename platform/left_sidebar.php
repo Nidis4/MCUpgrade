9b1c6b40d16ef20be76dbcf30dd249338de0966a
Harpreet Singh <er.hpreetsingh@gmail.com>
2018-03-15 10:20:13 +0530

    Fix Agent Action

--- platform/left_sidebar.php
+++ platform/left_sidebar.php
@@ -344,6 +344,22 @@
 				                            
 				                        </ul>
 				                    </li>
+				                     <li class="nav-parent">
+				                        <a class="nav-link" href="#">
+				                            <i class="fa fa-table" aria-hidden="true"></i>
+				                            <span>Agent actions</span>
+				                        </a>
+				                        <ul class="nav nav-children">
+				                            <li>
+				                                <a class="nav-link" href="pending_professionals.php">
+				                                    Pending Agent Verification
+				                                </a>
+				                            </li>
+				                            
+				                           
+				                            
+				                        </ul>
+				                    </li>
 				                    <li class="nav-parent">
 				                        <a class="nav-link" href="#">
 				                            <i class="fa fa-list-alt" aria-hidden="true"></i>
