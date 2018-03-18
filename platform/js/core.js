e779e32f2246af8d136c1ea610c1ac1a6d5fd667
hsingh051 <er.hpreetsingh@gmail.com>
2018-03-18 16:17:33 +0530

    Professional Profile Started

--- platform/js/core.js
+++ platform/js/core.js
@@ -1,5 +1,5 @@
 // Environment
-var API_LOCATION = 'http://upgrade.myconstructor.gr/webservices/api/';
+//var API_LOCATION = 'http://upgrade.myconstructor.gr/webservices/api/';
 
 // Localhost
-//var API_LOCATION = 'http://localhost/webservices/api/';
+var API_LOCATION = 'http://localhost/MCUpgrade/webservices/api/';
--- professional-profile/js/professional-profile.js
+++ professional-profile/js/professional-profile.js
@@ -1,3 +1,39 @@
+$("#loginForm").submit(function(){
+    
+    var getLoginAPI = API_LOCATION+'admin/login.php';
+
+    $.ajax({
+            type: "POST",
+            url: getLoginAPI,
+            data: {
+                username: $("#username").val(),
+                password: $("#password").val()
+            },
+            dataType: "json",
+            success: function(data)
+            {
+                var result = data['ResultCode'];
+                //alert(data[0]);
+                //alert (result);
+                if (result == '1') {
+                     // Send Ajax request to backend.php, with src set as "img" in the POST data
+                     var user = data['username'];
+                     var name = data['first_name'];
+                     var surname = data['last_name'];
+                     var id = data['id'];
+                     var fullname = name+" "+surname;
+                    $.post("session.php", {"user": user , "fullname":fullname, "id":id});
+
+                    window.location.replace('../platform/index.php');
+                }
+                else {
+                    alert("Έχετε πληκτρολογήσει λάθος στοιχεία. Παρακαλώ προσπαθήστε ξανά!");
+                    location.reload();
+                }
+            }
+        });
+    return false;
+});
 
     $(document).ready(function () {
         $('.mobile-menu').click(function(){
