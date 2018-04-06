bbcfebd7b0d16b478f60e44bdb7564de1467501b
Harpreet Singh <er.hpreetsingh@gmail.com>
2018-04-06 10:28:10 +0530

    login page design

--- login.php
+++ login.php
@@ -9,23 +9,23 @@
 			<div class="col-sm-12">
 				<p class="Sign-In">Συνδεθείτε στο<span> my</span><span class="mysign">Constructor</span><span>.gr</span></p>
 				<div class="new-sign-in-form">
-					<form action="/members/login" onsubmit="doLogin(); return false;" id="checkLoginForm" method="post" accept-charset="utf-8">
+					<form id="loginForm">
 						<div style="display:none;"><input name="_method" value="POST" type="hidden"></div>
 						<div class="sign-in-from">
 							<div class="form-group">
-								<input class="form-control" id="exampleInputEmail1" name="data[Member][email]" value="" placeholder="E-mail*" type="text">
+								<input class="form-control" id="exampleInputEmail1" name="username" value="" placeholder="E-mail*" type="text">
 							</div>
 							<div class="form-group">
-								<input class="form-control" id="exampleInputPassword1" placeholder="Κωδικός*" name="data[Member][password]" value="" type="password">
+								<input class="form-control" id="exampleInputPassword1" placeholder="Κωδικός*" name="pwd" value="" type="password">
 							</div>
 						</div>
 						<div class="col-md-12">
 							<div class="form-field-inner-part">
 								<div class="checkbox">
-									<label><input name="data[Member][done]" type="checkbox">Κρατήστε με συνδεδεμένο</label>
+									<label><input name="rememberme" type="checkbox">Κρατήστε με συνδεδεμένο</label>
 								</div>
 								<div class="Forgot_your">
-									<label for="exampleInputFile"><a href="https://myconstructor.gr/members/forgot_password">Ξεχάσατε τον κωδικό σας;</a></label>
+									<label for="exampleInputFile"><a href="<?php echo SITE_URL;?>professional-profile/forget_password.html">Ξεχάσατε τον κωδικό σας;</a></label>
 								</div>
 							</div>
 							<div class="login-btn">
@@ -48,20 +48,10 @@
 	</div><!-- top-sign-in-container-->
 </div>
 
-<script type="text/javascript">
 
-	$('.carousel').carousel({
-  		interval: 3000,
-  		pause: "false"
-	})
-
-
-
-
-</script>
 
 <script src="js/core.js" type="text/javascript"></script>
-<script src="js/sing-up-form.js" type="text/javascript"></script>
+<script src="professional-profile/js/professional-profile.js" type="text/javascript"></script>
 
 <?php 
 	include('footer.php');
