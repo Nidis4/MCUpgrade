28d3559509b75f6123c305f81cd2126d8bcfac5f
Heta <heta.vegda@gmail.com>
2018-05-03 19:59:05 +0530

    Prof. payment list

--- webservices/api/app/get_professionals_payment_list.php
+++ webservices/api/app/get_professionals_payment_list.php
@@ -5,27 +5,35 @@ $appointments_array = array();
 if(isset($_REQUEST['member_id']))
 {
     $member_id=isset($_REQUEST["member_id"]) ? $_REQUEST["member_id"] : "";
-    $payments = file_get_contents('http://upgrade.myconstructor.gr/webservices/api/payment/paymentByProfApi.php?prof_id='.$member_id);
+    $payments = file_get_contents('https://upgrade.myconstructor.gr/webservices/api/app/payment.php?prof_id='.$member_id);
     $paymentsPag = json_decode($payments, true);
-   // print_r($paymentsPag);
-    foreach ($paymentsPag as $payment) {
-        $obj['id'] = $payment['id'];
-        $obj['amount'] = $payment['amount'];
-        $obj['name'] = $payment['name'];
-        $obj['address'] = $payment['address'];
-        $obj['budget'] = $payment['budget'];
-        $obj['commision'] = $payment['commision'];
-        $obj['datetimeCreated'] = $payment['datetime_added'];
-        $obj['status2'] = $payment['status'];
-        $obj['Balance'] = $payment['balance'];
-        $obj['date'] = $payment['date'];
-        $obj['time'] = $payment['time'];
-        $appointments_array[]=$obj; 
-
+    //print_r($paymentsPag);
+    if (array_key_exists("main_status",$paymentsPag))
+    {
+        $row_array['status'] = "0";
+        $row_array['data'] = $appointments_array;
+        echo json_encode($row_array);
     }
-    $row_array['status'] = "1";
-    $row_array['data'] = $appointments_array;
-    echo json_encode($row_array);
+    else{
+        foreach ($paymentsPag as $payment) {
+            $obj['id'] = $payment['id'];
+            $obj['amount'] = $payment['amount'];
+            $obj['name'] = $payment['name'];
+            $obj['address'] = $payment['address'];
+            $obj['budget'] = $payment['budget'];
+            $obj['commision'] = $payment['commision'];
+            $obj['datetimeCreated'] = $payment['datetime_added'];
+            $obj['status2'] = $payment['status2'];
+            $obj['Balance'] = $payment['balance'];
+            $obj['date'] = $payment['date'];
+            $obj['time'] = $payment['time'];
+            $appointments_array[]=$obj; 
+
+        }
+        $row_array['status'] = "1";
+        $row_array['data'] = $appointments_array;
+        echo json_encode($row_array);
+    }
 
 }
 else{
